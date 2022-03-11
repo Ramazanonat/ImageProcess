df038ce1affc05f37a6254c011b261e4f9ae758c
ramazan <ramazan.onat@tsoft.com.tr>
2022-03-11 18:03:01 +0300

    düzenlemeler

--- TakeScreenShot.py
+++ TakeScreenShot.py
@@ -1,12 +1,50 @@
 import numpy as np
 import cv2
 from PIL import ImageGrab
+from PIL import Image, ImageEnhance
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imshow, imread
+from skimage.color import rgb2yuv, rgb2hsv, rgb2gray, yuv2rgb, hsv2rgb
+from scipy.signal import convolve2d
 # left_x, top_y, right_x, bottom_y
-coordinates = {(1170, 530, 1270, 630), (1060, 530, 1150, 630), (950, 530, 1050, 630), (850, 530, 950, 630)}
+coordinates = {(1308, 635, 1333, 660), (1331, 635, 1356, 660), (1355, 635, 1380, 660)}
+
+time.sleep(5)
 
 for idx, val in enumerate(coordinates):
     img = ImageGrab.grab(bbox=(val))  # x, y, w, h
+
+    img = img.resize((50, 50))
+    enhancer = ImageEnhance.Contrast(img)
+    factor = 1.9  # decrease constrast
+    img = enhancer.enhance(factor)
+
     img_np = np.array(img)
-    cv2.imwrite('icons/' + str(idx) + '.png', img_np)
+    img = cv2.fastNlMeansDenoisingColored(img_np, None, 10, 10, 7, 21)
+    cv2.imwrite('icons/' + str(idx) + '.png', img_np),
     if cv2.waitKey(1) & 0Xff == ord('q'):
         break
+
+
+# def convolver_rgb(image, kernel, iterations=1):
+    # img_yuv = rgb2yuv(image)
+    # img_yuv[:, :, 0] = multi_convolver(img_yuv[:, :, 0], kernel,
+    #                                    iterations)
+    # final_image = yuv2rgb(img_yuv)
+    #
+    # fig, ax = plt.subplots(1, 2, figsize=(17, 10))
+    #
+    # ax[0].imshow(image)
+    # ax[0].set_title(f'Original', fontsize=20)
+    #
+    # ax[1].imshow(final_image);
+    # ax[1].set_title(f'YUV Adjusted, Iterations = {iterations}',
+    #                 fontsize=20)
+    #
+    # [axi.set_axis_off() for axi in ax.ravel()]
+    #
+    # fig.tight_layout()
+    #
+    # return final_image
--- Test.py
+++ Test.py
@@ -1,17 +1,33 @@
-
 import os
 import cv2
+from PIL import Image, ImageEnhance
+import numpy as np
 
 path = "icons"
 dir_list = sorted(filter(lambda x: os.path.isfile(os.path.join(path, x)), os.listdir(path)))
 
+# while 1:
 for idx, val in enumerate(dir_list):
-    method = cv2.TM_SQDIFF_NORMED
+    # TM_CCOEFF
+    # TM_CCOEFF_NORMED
+    # TM_CCORR -
+    # TM_CCORR_NORMED -
+    # TM_SQDIFF -
+    # TM_SQDIFF_NORMED -
+    method = cv2.TM_CCORR_NORMED
 
     # Read the images from the file
     small_image = cv2.imread('icons/' + str(dir_list[idx]))
-    large_image = cv2.imread('big.png')
+    large_image = cv2.imread('big3.png')
 
+    # *****
+    # enhancer = ImageEnhance.Contrast(large_image)
+    # factor = 1.9  # decrease constrast
+    # # large_image = enhancer.enhance(factor)
+    # #
+    # # img_np = np.array(large_image)
+    # # large_image = cv2.fastNlMeansDenoisingColored(img_np, None, 10, 10, 7, 21)
+    # # ****
     result = cv2.matchTemplate(small_image, large_image, method)
 
     # We want the minimum squared difference
@@ -33,7 +49,7 @@ for idx, val in enumerate(dir_list):
     cv2.rectangle(large_image, (MPx, MPy), (MPx + tcols, MPy + trows), (0, 0, 255), 2)
 
     # Display the original image with the rectangle around the match.
-    # cv2.imshow('output', large_image)
+    cv2.imshow('output', large_image)
 
     # The image is only displayed if we call this
-    # cv2.waitKey(0)
+    cv2.waitKey(0)
